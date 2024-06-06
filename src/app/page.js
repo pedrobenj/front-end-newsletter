2b00b28596dd9e664f43e33cbfa4d6660ee7a2d5
PEDRO HENRIQUE DE LIMA BENJAMIN <PEDRO0762@DIGITALCOLLEGE.LOCAL>
2024-06-05 21:52:02 -0300

    finalizando a aplicação

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
-import 'react-toastify/dist/ReactToastify.css';
-import { toast, ToastContainer } from "react-toastify";
+import toast, { Toaster } from "react-hot-toast";
 
 export default function Home() {
 
@@ -11,42 +10,45 @@ export default function Home() {
 
   function onSubmit() {
     const myHeaders = new Headers();
-myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Content-Type", "application/json");
 
-const raw = JSON.stringify({
-  "name": name,
-  "email": email
-});
+    const raw = JSON.stringify({
+      "name": name,
+      "email": email
+    });
 
-const requestOptions = {
-  method: "POST",
-  headers: myHeaders,
-  body: raw,
-  redirect: "follow"
-};
+    const requestOptions = {
+      method: "POST",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow"
+    };
 
-fetch("http://localhost:3001/email/register-email", requestOptions)
-  .then((response) => response.text())
-  .then((result) => console.log(result))
-  .catch((error) => console.error(error));
+    fetch("http://localhost:3001/email/register-email", requestOptions)
+      .then((response) => {
+        toast.success("Inscrição realizada com sucesso!")
+        return response.text()
+      })
+      .catch((error) => {
+        toast.error("Erro ao realizar inscrição!")
+      });
   }
-
   return (
     <main className="flex min-h-screen flex-col items-center justify-center">
-      <ToastContainer />
-      <div className="h-[28rem] p-5 w-2/3 bg-cyan-600 rounded-lg flex">
-        <div className="relative flex justify-center items-center bg-indigo-500 flex-[2] min-h-full rounded">
-          <Image src="https://cdn-icons-png.flaticon.com/512/4939/4939994.png
+        <div className="h-[28rem] p-5 w-2/3 bg-cyan-600 rounded-lg flex">
+          <div className="relative flex justify-center items-center bg-indigo-500 flex-[2] min-h-full rounded">
+            <Image src="https://cdn-icons-png.flaticon.com/512/4939/4939994.png
           "alt="hero" layout="fill" objectFit="contain" className="rounded max-h-36 flex mt-20" />
+          </div>
+          <div className="flex-[3] px-16 min-h-full flex flex-col text-white justify-center items-center gap-5">
+            <h1 className="text-2xl">Se inscreva para receber novidades</h1>
+            <p className="text-lg">Nunca mais se esqueça de comprar seus insumos semanais com qualidade de vida!</p>
+            <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Digite seu nome" className="w-full p-2 rounded text-black" />
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Digite seu email" className="w-full p-2 rounded text-black" />
+            <button onClick={onSubmit} className="bg-indigo-500 p-4 rounded min-w-full">Inscrever</button>
+          </div>
         </div>
-        <div className="flex-[3] px-16 min-h-full flex flex-col text-white justify-center items-center gap-5">
-          <h1 className="text-2xl">Se inscreva para receber novidades</h1>
-          <p className="text-lg">Nunca mais se esqueça de comprar seus insumos semanais com qualidade de vida!</p>
-          <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder="Digite seu nome" className="w-full p-2 rounded text-black" />
-          <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="Digite seu email" className="w-full p-2 rounded text-black" />
-          <button onClick={onSubmit} className="bg-indigo-500 p-4 rounded min-w-full">Inscrever</button>
-        </div>
-      </div>
+      <Toaster />
     </main>
   );
 }
