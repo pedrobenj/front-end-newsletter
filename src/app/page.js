5e4326a8eb3e2d08a88fcc7218af34e5c2f845c5
PEDRO HENRIQUE DE LIMA BENJAMIN <PEDRO0762@DIGITALCOLLEGE.LOCAL>
2024-06-05 21:23:43 -0300

    upando o front versão 2.0

--- src/app/page.js
+++ src/app/page.js
@@ -11,30 +11,24 @@ export default function Home() {
 
   function onSubmit() {
     const myHeaders = new Headers();
-    myHeaders.append("Content-Type", "application/json");
+myHeaders.append("Content-Type", "application/json");
 
+const raw = JSON.stringify({
+  "name": name,
+  "email": email
+});
 
-    try {
-      const r = fetch('http://localhost:3001/email/register-email-on-newsletter', {
-      method: 'POST',
-      headers: myHeaders,
-      body: JSON.stringify({
-        email,
-        name
-      }),
-    })
+const requestOptions = {
+  method: "POST",
+  headers: myHeaders,
+  body: raw,
+  redirect: "follow"
+};
 
-    if (r.status !== 200) {
-      throw new Error('Erro ao se inscrever, tente novamente mais tarde!')
-    }
-
-    setEmail('')
-    setName('')
-
-      toast.success('Inscrição realizada com sucesso!')
-    } catch (error) {
-      toast.error('Erro ao se inscrever, tente novamente mais tarde!')
-    }
+fetch("http://localhost:3001/email/register-email", requestOptions)
+  .then((response) => response.text())
+  .then((result) => console.log(result))
+  .catch((error) => console.error(error));
   }
 
   return (
@@ -43,7 +37,7 @@ export default function Home() {
       <div className="h-[28rem] p-5 w-2/3 bg-cyan-600 rounded-lg flex">
         <div className="relative flex justify-center items-center bg-indigo-500 flex-[2] min-h-full rounded">
           <Image src="https://cdn-icons-png.flaticon.com/512/4939/4939994.png
-" alt="hero" layout="fill" objectFit="contain" className="rounded max-h-36 flex mt-20" />
+          "alt="hero" layout="fill" objectFit="contain" className="rounded max-h-36 flex mt-20" />
         </div>
         <div className="flex-[3] px-16 min-h-full flex flex-col text-white justify-center items-center gap-5">
           <h1 className="text-2xl">Se inscreva para receber novidades</h1>
